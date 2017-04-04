b1955d240b146e2f211d21b7a987e028ff6fa2f1
Fred Bellingham <fbgithub@gmail.com>
2017-04-04 12:51:04 -0400

    R Programming: Week 3, Programming Assignment 2. The functions makeCacheMatrix & cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## makeCacheMatrix creates an object of type makeCacheMatrix() that 
+## makeCacheMatrix creates an object of type makeCacheMatrix() that
 ## stores a matrix (provided as a formal parameter by user) & provides
 ## 4 methods for operating on the object. [1] set() allows the user to
 ## reset the matrix. [2] get() allows the user to get the value of the matrix.
-## [3] setinv() allows the function cacheSolve() to cache the inverse of the 
-## matrix. [4] getinv allows the user to get the value of the local var 'inv', 
-## which is the inverse that's cached by cacheSolve() when cacheSolve() is 
-## given an argument of type makeCacheMatrix().  
+## [3] setinv() allows the function cacheSolve() to cache the inverse of the
+## matrix. [4] getinv allows the user to get the value of the local var 'inv',
+## which is the inverse that's cached by cacheSolve() when cacheSolve() is
+## given an argument of type makeCacheMatrix().
 
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
@@ -24,16 +21,16 @@ makeCacheMatrix <- function(x = matrix()) {
              getinv = getinv)
 }
 
-## cachesolve() caches the inverse of the matrix stored in the object of 
-## type makeCahceMatrix(). It's assumed that the matrix provided as a formal
-## param to makeCacheMatrix is always invertible. See previous comment for 
+## cacheSolve() caches the inverse of the matrix stored in the object of
+## type makeCacheMatrix(). It's assumed that the matrix provided as a formal
+## param to makeCacheMatrix is always invertible. See previous comment for
 ## further details. Example use of these 2 functions:
 ##
-##	matrix1 <- matrix(c(2,2,3,2), nrow = 2)
-##	myMatrix <- makeCacheMatrix(matrix1)
-##	cachesolve(myMatrix)
+##      matrix1 <- matrix(c(2,2,3,2), nrow = 2)
+##      myMatrix <- makeCacheMatrix(matrix1)
+##      cacheSolve(myMatrix)
 
-cachesolve <- function(x, ...) {
+cacheSolve <- function(x, ...) {
         inv <- x$getinv()
         if(!is.null(inv)) {
                 message("getting cached data")
